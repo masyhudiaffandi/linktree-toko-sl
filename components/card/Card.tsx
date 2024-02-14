e7cf36e648576a0cda37c9a873e9f7c27aae473b
masyhudiaffandi <masyhudiaffandi816@gmail.com>
2024-02-14 16:29:55 +0700

    feat : added lazada

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -16,13 +16,18 @@ const Toko = [
     "name": "TokoPedia",
     "link": "www.tokopedia.com",
     "image": "/public/toko.png"
+  },
+  {
+    "name": "Lazada",
+    "link": "www.lazada.com",
+    "image": "/public/toko.png"
   }
 ]
 
 export default function App() {
   return (
     // map TOKO
-    <div className="flex flex-col justify-center items-center gap-8">
+    <div className="flex flex-col justify-center items-center gap-4 w-[100vh]">
       {Toko.map((toko) => (
         <Card key={toko.name} className="max-w-[340px] flex justify-between items-center w-[100vw] py-3 rounded-full">
           <div className="flex items-center justify-between gap-10">
