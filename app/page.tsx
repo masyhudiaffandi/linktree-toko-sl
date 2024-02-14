968a27febee6514838eff8bc307457c2005d79d3
masyhudiaffandi <masyhudiaffandi816@gmail.com>
2024-02-14 22:18:54 +0700

    feat : aurora effect

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,11 @@ export default function Home() {
       <div className="relative h-screen overflow-hidden">
         <div className="absolute inset-0 overflow-hidden">
           <div className="absolute inset-0">
-            <div className="absolute top-1/4 left-1/4 w-64 h-64 bg-green-500 rounded-full mix-blend-screen opacity-50 filter blur-xl"></div>
-            <div className="absolute top-1/2 right-1/4 w-64 h-64 bg-pink-500 rounded-full mix-blend-screen opacity-50 filter blur-xl"></div>
+            <div className="absolute top-[396px] left-1/2 w-32 h-48 bg-green-500 rounded-full mix-blend-screen opacity-50 filter blur-[90px]"></div>
+            <div className="absolute top-[28px] right-1/4 w-64 h-48 bg-pink-500 rounded-full mix-blend-screen opacity-50 filter blur-[90px]"></div>
           </div>
         </div>
-        <div className="card-container flex justify-center items-center">
+        <div className="card-container flex justify-center items-center mt-8">
           <Card />
         </div>
       </div>
